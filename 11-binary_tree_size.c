2e1fda89a78a70852a6618381103e13b4ab5d87e
abenixo <abenezer.ashenafi.alx@gmail.com>
2022-11-14 14:01:43 -0800

    kol

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,34 @@
+#include"binary_trees.h"
+/**
+ * binary_tree_size - calculates the size of the binary tree
+ * @tree: pointer to the root node of the tree to measure the size
+ * Return: return the size interms of size_t data type
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if(tree == NULL)
+	{
+		return (0);
+	}
+
+	size_t size = sizecal(tree) + 1;
+	return (size);
+}
+/**
+ * sizecal - calculate the size of the binary tree
+ * @tree: pointer to the root node the tree to measure the size
+ * Return: return the size interms of int type
+ */
+int sizecal(const binary_tree_t *tree)
+{
+	if(tree == NULL)
+	{
+		return (-1);
+	}
+
+	int sizel = 1, sizer = 1;
+	sizel += sizecal(tree->left);
+	sizer += sizecal(tree->right);
+	return (sizel + sizer);
+
+}
