061e8d510db8fc28077c78bb2f7877c5319ecaad
abenixo <abenezer.ashenafi.alx@gmail.com>
2022-11-13 23:50:21 -0800

    fourth

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,25 @@
+#include"binary_trees.h"
+/**
+ * binary_tree_is_leaf - check whther a node is aleaf or not
+ * @node: a pointer to a node to check
+ * @Retrun: 1 if the node is a leaf node
+ * Other wise return 0
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if(node == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		if((node->left == NULL) && (node->right == NULL))
+		{
+			return 1;
+		}
+		else
+		{
+			return 0;
+		}
+	}
+}
