982f6432d9bd8e25f5b163b566de868c8d284c48
abenixo <abenezer.ashenafi.alx@gmail.com>
2022-11-13 23:51:24 -0800

    fifth

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,18 @@
+#include"binary_trees.h"
+/**
+* binary_tree_is_root - check a node is root or not
+* @node: is a pointer to the node to check
+* Return: 1 if it root node.
+* Otherwise - it return 0.
+*/
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if((node != NULL) && (node->parent == NULL))
+	{
+		return (1);
+	}
+	
+	return (0);
+
+
+}
