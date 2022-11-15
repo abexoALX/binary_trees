461dc7e1e17ae3075e6e26a9d6b4a57beea62e2a
abenixo <abenezer.ashenafi.alx@gmail.com>
2022-11-15 00:32:12 -0800

    insert left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -27,6 +27,12 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		parent->left->parent = new_node;
 	}
 
+	else
+	{
+		new_node->left == NULL;
+
+	}
+
 	parent->left = new_node;
 
 	return (new_node);
