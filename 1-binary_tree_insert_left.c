f8b405494382ab94756f884ca6cdc0241c22ff1a
abenixo <abenezer.ashenafi.alx@gmail.com>
2022-11-15 00:56:14 -0800

    insert left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,8 +10,10 @@
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
+	binary_tree_t *tmp;
 
 	new_node = malloc(sizeof(binary_tree_t));
+	tmp = malloc(sizeof(binary_tree_t));
 
 	if (parent == NULL)
 		return (NULL);
@@ -29,7 +31,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	else
 	{
 		parent->left = new_node;
-		new_node->left == NULL;
+		new_node->left = NULL;
 	}
 	new_node->n = value;
 	new_node->parent = parent;
