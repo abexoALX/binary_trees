1d52c741e082909600809427a3c02288c7cb73e0
abenixo <abenezer.ashenafi.alx@gmail.com>
2022-11-15 04:38:57 -0800

    preorder

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -5,7 +5,6 @@
  *@tree : is a pointer to the root node of the tree to traverse
  *@func : is a pointer to a function to call for each node.
 */
-
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL)
