2a451fd35439989f8e480b0b035701e6b521a70a
abenixo <abenezer.ashenafi.alx@gmail.com>
2022-11-16 06:40:43 -0800

    leaves

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -12,8 +12,8 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	if (tree)
 	{
 		leaves += (!tree->left) && (!tree->right) ? 1 : 0;
-		 binary_tree_leaves(tree->left);
-		 binary_tree_leaves(tree->right);
+		 leaves += binary_tree_leaves(tree->left);
+		 leaves += binary_tree_leaves(tree->right);
 	}
 	return (leaves);
 }
