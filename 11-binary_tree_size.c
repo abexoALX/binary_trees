3083395d764a943fc6a74d6bdfc44b0960903c20
abenixo <abenezer.ashenafi.alx@gmail.com>
2022-11-16 05:24:59 -0800

    leaf

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,34 +1,23 @@
 #include "binary_trees.h"
+
 /**
  * binary_tree_size - calculates the size of the binary tree
  * @tree: pointer to the root node of the tree to measure the size
- * Return: return the size interms of size_t data type
+ * Return: If tree is NULL, your function must return 0, else return size.
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	if(tree == NULL)
+	if (tree)
 	{
-		return (0);
+		size_t sizel = 0, sizer = 0;
+		
+		size_t max = 0;
+
+		sizel = tree->left ? binary_tree_size(tree->left) + 1 : 0;
+		sizer = tree->right ? binary_tree_size(tree->right) + 1 : 0;
+		max = sizel + sizer;
+		return (max);
 	}
 
-	size_t size = sizecal(tree) + 1;
-	return (size);
-}
-/**
- * sizecal - calculate the size of the binary tree
- * @tree: pointer to the root node the tree to measure the size
- * Return: return the size interms of int type
- */
-int sizecal(const binary_tree_t *tree)
-{
-	if(tree == NULL)
-	{
-		return (-1);
-	}
-
-	int sizel = 1, sizer = 1;
-	sizel += sizecal(tree->left);
-	sizer += sizecal(tree->right);
-	return (sizel + sizer);
-
+	return (0);
 }
--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,8 +1,9 @@
 #include "binary_trees.h"
+
 /**
  * binary_tree_leaves - calucalte the leaves of anode
  * @tree: pointer to the root node of the tree to count the number of leaves
- * Return: returnsthe leaves of a node interms of size_t data type
+ * Return: If tree is NULL, your function must return 0, else 2 or 1.
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
@@ -12,12 +13,12 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	}
 	size_t leaves = 0;
 
-	if(tree->left != NULL)
+	if (tree->left != NULL)
 	{
 		leaves++;
 	}
 
-	if(tree->right != NULL)
+	if (tree->right != NULL)
 	{
 		leaves++;
 	}
