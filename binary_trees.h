4f9db4a1c07c87b3670c517b9a1b79e081bc3f56
abexoALX <abenezer.ashenafi.alx@gmail.com>
2022-11-16 21:54:01 +0300

    header

--- binary_trees.h
+++ binary_trees.h
@@ -50,4 +50,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 int sizecal(const binary_tree_t *tree);
 size_t max(size_t num1, size_t num2);
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
+
+
 #endif /* _BINARY_TREES_H_ */
