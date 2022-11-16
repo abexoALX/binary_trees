105ee57ce04b674ef023b7a7eb135e5efffdd8e7
abenixo <abenezer.ashenafi.alx@gmail.com>
2022-11-16 02:04:44 -0800

    height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,17 +1,6 @@
 #include"binary_trees.h"
 
 /**
- * max - calculates the maximum between the two nodes
- * @num1: the first number to be compared
- * @num2: the second number to be compared
- * Return: returns the largest between the two values
- */
-size_t max(size_t num1, size_t num2)
-{
-	return (num1 >= num2 ? num1 : num2);
-
-}
-/**
  * binary_tree_height - mesure the height of the binary tree
  * @tree : pointer to the root node of the tree to measure the height
  * Return: return the hieght of the binary tree size_t or else null
@@ -26,5 +15,5 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	size_t r = (tree->right) ? (1 + binary_tree_height(tree->right)) : 0;
 
-	return (max(l, r));
+	return (l > r ? l : r);
 }
