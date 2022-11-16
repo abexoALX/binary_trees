cb3c115de5fdcdd7f3c070b0b19c920c18ab4e6a
abexoALX <abenezer.ashenafi.alx@gmail.com>
2022-11-16 21:58:38 +0300

    level

--- binary_trees.h
+++ binary_trees.h
@@ -51,6 +51,6 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 int sizecal(const binary_tree_t *tree);
 size_t max(size_t num1, size_t num2);
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
-
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 
 #endif /* _BINARY_TREES_H_ */
