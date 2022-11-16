2cb39fdd8041eee8d8a6d664cab57bfa2cb3b54e
abexoALX <abenezer.ashenafi.alx@gmail.com>
2022-11-16 22:38:41 +0300

    header

--- binary_trees.h
+++ binary_trees.h
@@ -58,5 +58,9 @@ int binary_tree_is_bst(const binary_tree_t *tree);
 bst_t *bst_insert(bst_t **tree, int value);
 int binary_tree_is_avl(const binary_tree_t *tree);
 bst_t *array_to_bst(int *array, size_t size);
+int binary_tree_balance(const binary_tree_t *tree);
+int binary_tree_is_full(const binary_tree_t *tree);
+int binary_tree_is_perfect(const binary_tree_t *tree);
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 
 #endif /* _BINARY_TREES_H_ */
