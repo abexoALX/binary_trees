66e84a9fa7e8b68284b8a2cc21702fc6d14f83d7
abenixo <abenezer.ashenafi.alx@gmail.com>
2022-11-16 02:32:48 -0800

    depth

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,31 +1,34 @@
 #include "binary_trees.h"
+
 /**
- * binary_tree_depth - caluculate the depth of the binary tree from the specified node
+ * binary_tree_depth - depth of the binary tree from the specified node
  * @tree: pointer to the node to measure the depth
- * Return: the depth in terms of size_t
+ * Return: NULL if tree is null else the depth in terms of size_t
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	if(tree == NULL)
+	if (tree == NULL)
 	{
 		return (0);
 	}
+	size_t depth = 0;
 
-	size_t depth = depthcalc(tree);
+	depth = depthcalc(tree);
 	return (depth);
 }
 /**
  * depthcalc - calculate the depth of the binary tree
  * @tree: pointer to the node to measure the depth
- * Return: return the depth in int data type
+ * Return: NULL if tree is null else the depth in int data type
  */
 int depthcalc(const binary_tree_t *tree)
 {
-	if(tree == NULL )
+	if (tree == NULL)
 	{
 		return (-1);
 	}
+	int depth = 0;
 
-	int depth = depthcalc(tree->parent) + 1;
-	return (depth);	
+	depth = depthcalc(tree->parent) + 1;
+	return (depth);
 }
